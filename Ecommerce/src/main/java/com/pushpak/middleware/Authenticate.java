45ef8deaf23d6fedf37f80625f4eb58979809ee7
ninavepushpak <pushpakninave.jee@gmail.com>
2025-06-11 09:54:12 +0530

    created ecom project

---
+++ Ecommerce/src/main/java/com/pushpak/middleware/Authenticate.java
@@ -0,0 +1,41 @@
+package com.pushpak.middleware;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class Authenticate
+ */
+@WebServlet("/authenticate")
+public class Authenticate extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		String userName = request.getParameter("userName");
+		String password = request.getParameter("password");
+		
+		if(userName.equals("dac") && password.equals("dac")) {
+			out.println("Welcome to the online shopping site");
+		}else {
+			out.println("<font color='red'>Invalid username</font>");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
