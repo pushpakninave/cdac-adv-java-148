1d4ac2a22cc8725f052a828b11d5eac17be7f2a3
ninavepushpak <pushpakninave.jee@gmail.com>
2025-06-11 12:37:36 +0530

    added basic authentication with jdbc

--- Ecommerce/src/main/java/com/pushpak/middleware/Authenticate.java
+++ Ecommerce/src/main/java/com/pushpak/middleware/Authenticate.java
@@ -7,6 +7,11 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  * Servlet implementation class Authenticate
@@ -23,11 +28,48 @@ public class Authenticate extends HttpServlet {
 		String userName = request.getParameter("userName");
 		String password = request.getParameter("password");
 		
-		if(userName.equals("dac") && password.equals("dac")) {
-			out.println("Welcome to the online shopping site");
-		}else {
-			out.println("<font color='red'>Invalid username</font>");
-		}
+		
+		// Database resources
+	    Connection dbConnection = null;
+	    PreparedStatement authUserStatement = null;
+	    ResultSet res = null;
+	    
+	    try {
+	        // Load driver and establish connection
+	        Class.forName("com.mysql.cj.jdbc.Driver");
+	        dbConnection = DriverManager.getConnection(
+	            "jdbc:mysql://localhost:3306/cdac", "root", "pushpak123");
+	        
+	        // Secure query with prepared statement
+	        String sql = "SELECT 1 FROM user_info WHERE uname=? AND pwd=?";
+	        authUserStatement = dbConnection.prepareStatement(sql);
+	        authUserStatement.setString(1, userName);
+	        authUserStatement.setString(2, password);
+	        
+	        res = authUserStatement.executeQuery();
+	        
+	        if (res.next()) {
+	            out.println("Welcome to the online shopping site");
+	        } else {
+	            out.println("<font color='red'>Invalid username or password</font>");
+	        }
+	        
+	    } catch (ClassNotFoundException e) {
+	        out.println("<font color='red'>Database driver not found</font>");
+	        e.printStackTrace();
+	    } catch (SQLException e) {
+	        out.println("<font color='red'>Database error occurred</font>");
+	        e.printStackTrace();
+	    } finally {
+	        // Proper resource cleanup
+	        try {
+	            if (res != null) res.close();
+	            if (authUserStatement != null) authUserStatement.close();
+	            if (dbConnection != null) dbConnection.close();
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	        }
+	    }
 	}
 
 	/**
